af62a732db485d3935a68af3e7f39ebcac39e949
davidhitman <hitimanadav47@gmail.com>
2026-02-05 12:00:31 +0200

    Change to enable test database

---
+++ src/test/java/com/example/dalyda_backend_stockmanager/DalydaBackendStockManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.dalyda_backend_stockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class DalydaBackendStockManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
